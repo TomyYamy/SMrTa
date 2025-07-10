c72c8a0baaa06a804f56b5b2b9098df65f65452c
Tomoya Yamaguchi <tomoya_yamaguchi@mail.toyota.co.jp>
2025-07-10 09:13:25 +0900

    add medicine_room

--- benchmarks/potaro/generate_potaro_weighted_graph.py
+++ benchmarks/potaro/generate_potaro_weighted_graph.py
@@ -2,7 +2,24 @@ import pickle
 
 # Generate
 potaro_weighted_graph = {}
+## from ME_room: 0
 potaro_weighted_graph[0] = { 2: 5,
+                             3: 5,
+                             4: 5,
+                             5: 5,
+                             6: 5,
+                             7: 5,
+                             8: 5,
+                             9: 5,
+                            10: 5,
+                            11: 5,
+                            12: 5,
+                            13: 5,
+                            14: 5,
+                            15: 5,
+                            16: 5}
+## from medicine_room: 1
+potaro_weighted_graph[1] = { 2: 5,
                              3: 5,
                              4: 5,
                              5: 5,

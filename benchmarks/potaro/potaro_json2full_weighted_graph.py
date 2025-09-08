0ba61da02baa3d68cb7b672953abcf3edeb113db
Tomoya Yamaguchi <tomoya_yamaguchi@mail.toyota.co.jp>
2025-09-08 11:46:46 +0900

    calc dist

--- benchmarks/potaro/potaro_json2full_weighted_graph.py
+++ benchmarks/potaro/potaro_json2full_weighted_graph.py
@@ -14,25 +14,28 @@ if __name__ == '__main__':
 
   # Convert to NetworkX graph
   ## weighted_elist
-  G = nx.Graph()
+  DiG = nx.DiGraph()
   for element_dict in weighted_graph_list:
-    G.add_edge(element_dict['start_global_id'], element_dict['goal_global_id'] , weight = element_dict['average_elapsed_time'],
+    DiG.add_edge(element_dict['start_global_id'], element_dict['goal_global_id'] , weight = element_dict['average_elapsed_time'],
                start_global_id = element_dict['start_global_id'],
                goal_waypoint_name = element_dict['goal_waypoint_name'],
                elapsed_times = element_dict['elapsed_times'])
   #print(G)
 
   # Run Dijkstra
-  path_nodes = nx.dijkstra_path(G, '020100310', '020100603', weight='weight')
+  path_nodes = nx.dijkstra_path(DiG, '020100310', '020100603', weight='weight')
   edges_path_list = [(path_nodes[i], path_nodes[i+1]) for i in range(len(path_nodes)-1)]
   print('shortest path =', edges_path_list)
+  edge_weights = nx.get_edge_attributes(DiG, 'weight')
+  #print(edge_weights)
+  dist = sum([edge_weights[edge] for edge in edges_path_list])
+  print('dist =', dist)
 
   # Draw
   fig, ax = plt.subplots(figsize=(20,10))
-  node_positions = nx.spring_layout(G, scale=50)# auto layout
-  nx.draw_networkx(G, pos=node_positions, node_color='lightgrey', node_size=100, width=1)
-  edge_labels = nx.get_edge_attributes(G, 'weight')
-  nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=edge_labels)
+  node_positions = nx.spring_layout(DiG, scale=50)# auto layout
+  nx.draw_networkx(DiG, pos=node_positions, node_color='lightgrey', node_size=100, width=1)
+  nx.draw_networkx_edge_labels(DiG, pos=node_positions, edge_labels=edge_weights)
   plt.axis('off')
   ax.set(aspect=1)
   #plt.savefig(prefix_file_name+'.pdf')

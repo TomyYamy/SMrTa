8f8d59ee1edaa0ac09fb63b1fab50b3ab24f9d3c
Tomoya Yamaguchi <tomoya_yamaguchi@mail.toyota.co.jp>
2025-09-11 18:36:59 +0900

    add task match

--- benchmarks/potaro/generate_tasks_from_potaro_database.py
+++ benchmarks/potaro/generate_tasks_from_potaro_database.py
@@ -8,7 +8,7 @@
 # order_start_time
 # status
 # od_list
-# transport_type
+# transport_type = {1: 薬剤、検体 }
 # reservation_datetime
 # metomass_reservation_id
 # device_seq_number
@@ -20,6 +20,23 @@
 # created_user
 # modified
 # modified_user
+#
+# - transport_tasks
+# task_id
+# order_id
+# sequence_number
+# task_sequence_total_count
+# device_id
+# task_type
+# task_object
+# destination
+# task_status
+# deleted
+# created
+# created_user
+# modified
+# modified_user
+
 
 import datetime
 import json
@@ -52,8 +69,16 @@ if __name__ == '__main__':
   for transport_order in transport_orders_date_filtered:
     created = datetime.datetime.strptime(transport_order['created'], '%Y-%m-%d %H:%M:%S')
     transport_desired_time = datetime.datetime.strptime(transport_order['transport_desired_time'], '%Y-%m-%d %H:%M:%S')
-    print(f'issued time={created} deadline={transport_desired_time}')
-    print(transport_order['od_list'])
+    #print(f'issued time={created} deadline={transport_desired_time}')
+    #print(transport_order['od_list'])
+
+    ## find order_id in transport_tasks
+    transport_tasks_matched = [ transport_task for transport_task in transport_tasks if transport_task['order_id'] == transport_order['order_id'] ]
+    if transport_order['transport_type'] == 1:
+      print(transport_order['transport_type'])
+      print(f'tasks num = {len(transport_tasks_matched)}')
+      print(transport_tasks_matched)
+    #print(transport_tasks_searched)
 
 
   #for transport_task in transport_tasks:

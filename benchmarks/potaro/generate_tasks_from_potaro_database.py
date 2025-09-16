e1df4a0006082606a9be8cb758171885aa225733
Tomoya Yamaguchi <tomoya_yamaguchi@mail.toyota.co.jp>
2025-09-16 22:13:27 +0900

    Revise test case

--- benchmarks/potaro/generate_tasks_from_potaro_database.py
+++ benchmarks/potaro/generate_tasks_from_potaro_database.py
@@ -127,3 +127,13 @@ if __name__ == '__main__':
   print(f'--test case # is {len(test_case)} --')
   for order in test_case:
     print(order)
+
+  # Revise test case
+  for order in test_case:
+    ## add large value when the dead line is too tight.
+    if order['deadline'] - order['issued_time'] < datetime.timedelta(minutes=1):
+      order['deadline'] = order['issued_time'] + datetime.timedelta(hours=2)
+
+  print(f'--test case rev # is {len(test_case)} --')
+  for order in test_case:
+    print(order)

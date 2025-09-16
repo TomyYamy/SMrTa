2fd195d01528602fe7d2187faa4d728d5b01e4d8
Tomoya Yamaguchi <tomoya_yamaguchi@mail.toyota.co.jp>
2025-09-16 12:08:38 +0900

    add device filter

--- benchmarks/potaro/generate_tasks_from_potaro_database.py
+++ benchmarks/potaro/generate_tasks_from_potaro_database.py
@@ -3,12 +3,13 @@
 # - transport_order
 # order_id
 # device_id
+#        0001-011701-000005 011701系検体、　0001-011301-000002
 # ward_setting_id_list
 # transport_desired_time
 # order_start_time
 # status
 # od_list
-# transport_type = {1: 薬剤、検体 }
+# transport_type = {1:薬剤、検体随時便 2:スピッツ、検体定時便 3:ME搬送} 1 is anything else of 2 and 3. This time resolves 1 and 3.
 # reservation_datetime
 # metomass_reservation_id
 # device_seq_number
@@ -56,7 +57,7 @@ if __name__ == '__main__':
     transport_tasks = json.load(f)['transport_tasks']
   #print(transport_orders)
 
-  # filter orders
+  # Filter orders with Date
   filter_date = datetime.date(2025, 8, 20)
 
   transport_orders_date_filtered = []
@@ -66,19 +67,30 @@ if __name__ == '__main__':
       transport_orders_date_filtered.append(transport_order)
   #print(transport_orders_date_filtered)
 
+  # Filter task type. else specimen　-011701-
+  # device_id *-011701-* means specimen
+  transport_orders_device_id_filtered = []
   for transport_order in transport_orders_date_filtered:
+    if transport_order['device_id'] != None: #Note: sometimes the id has None...
+      if '-011701-' not in transport_order['device_id']:
+        transport_orders_device_id_filtered.append(transport_order)
+
+  # Resolve destination while matching transport_tasks
+  for transport_order in transport_orders_device_id_filtered:
     created = datetime.datetime.strptime(transport_order['created'], '%Y-%m-%d %H:%M:%S')
     transport_desired_time = datetime.datetime.strptime(transport_order['transport_desired_time'], '%Y-%m-%d %H:%M:%S')
+    #print(transport_order['transport_type'])
     #print(f'issued time={created} deadline={transport_desired_time}')
     #print(transport_order['od_list'])
 
     ## find order_id in transport_tasks
     transport_tasks_matched = [ transport_task for transport_task in transport_tasks if transport_task['order_id'] == transport_order['order_id'] ]
-    if transport_order['transport_type'] == 1:
+    if transport_order['transport_type'] == 3:
       print(transport_order['transport_type'])
+      print(transport_order['od_list'])
       print(f'tasks num = {len(transport_tasks_matched)}')
+      print('Matched tasks')
       print(transport_tasks_matched)
-    #print(transport_tasks_searched)
 
 
   #for transport_task in transport_tasks:
